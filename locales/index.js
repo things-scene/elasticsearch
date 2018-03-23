b1b36a938b27ed4c7fc560a6e98c28c387f8c2d4
Hearty Oh <heartyoh@hatiolab.com>
2018-03-23 11:33:32 +0900

    0.1.2

--- locales/index.js
+++ locales/index.js
@@ -2,16 +2,34 @@ export default {
   'en-US': {
     'keyword': 'keyword',
     'label.host': 'host',
-    'label.port': 'port'
+    'label.port': 'port',
+    'label.httpAuth': 'http-auth',
+    'label.index': 'index',
+    'label.types': 'types',
+    'label.query-string': 'query string',
+    'label.query-object': 'query object',
+    'label.log': 'log'
   },
   'ko-KR': {
     'keyword': '키워드',
     'label.host': '호스트명',
-    'label.port': '포트'
+    'label.port': '포트',
+    'label.httpAuth': 'HTTP 인증',
+    'label.index': 'index',
+    'label.types': 'types',
+    'label.query-string': '단순질의(q)',
+    'label.query-object': '질의객체(body)',
+    'label.log': 'log'
   },
   'zh-CN': {
     'keyword': 'keyword',
     'label.host': 'host',
-    'label.port': 'port'
+    'label.port': 'port',
+    'label.httpAuth': 'http-auth',
+    'label.index': 'index',
+    'label.types': 'types',
+    'label.query-string': 'query string',
+    'label.query-object': 'query object',
+    'label.log': 'log'
   }
 }
